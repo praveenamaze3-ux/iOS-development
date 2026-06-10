ac9a6a83c66900688d74f4de8226689bd41dbeff
PRAVEEN <praveenamaze3@gmail.com>
2026-06-10 22:02:29 +0530

    no message

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 let celsiusTEMP:Double = 25.0
 let fahrenheitTEMP=(celsiusTEMP*9/5)+32
 print("\(celsiusTEMP)°C is equal to \(fahrenheitTEMP)°F")
+
+for number in 1...100{
+    if number.isMultiple(of: 3) && number.isMultiple(of: 5){
+        print("FizzBuzz")
+    }
+    else if number.isMultiple(of: 3){
+        print("Fizz")
+    }
+    else if number.isMultiple(of: 5){
+        print("Buzz")
+    }
+    else{
+        print(number)
+    }
+}
