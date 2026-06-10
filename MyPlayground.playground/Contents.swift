dd2c6a4fd25f46b9dc62687d80b7aaee71e3bb02
PRAVEEN <praveenamaze3@gmail.com>
2026-06-10 22:06:08 +0530

    updated codes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,9 @@
+//1.conversion of celcius to fahrenheit:
 let celsiusTEMP:Double = 25.0
 let fahrenheitTEMP=(celsiusTEMP*9/5)+32
 print("\(celsiusTEMP)°C is equal to \(fahrenheitTEMP)°F")
 
+//2.print strings:
 for number in 1...100{
     if number.isMultiple(of: 3) && number.isMultiple(of: 5){
         print("FizzBuzz")
